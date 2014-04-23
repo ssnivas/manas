3f15fe40b06b7a4eda6c328a8ac6a95c499c4228
ssnivas <ssnivas@users.noreply.github.com>
2014-04-23 18:02:58 -0500

    Update Main.java

--- rest-sample/src/main/java/com/math/fibonacci/Main.java
+++ rest-sample/src/main/java/com/math/fibonacci/Main.java
@@ -15,7 +15,7 @@ import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
  */
 public class Main {
     // Base URI the Grizzly HTTP server will listen on
-    public static final String BASE_URI = "http://localhost:8080/rest-sample";
+    public static final String BASE_URI = "http://localhost:8080/rest-sample/";
 
     /**
      * Starts Grizzly HTTP server exposing JAX-RS resources defined in this application.
